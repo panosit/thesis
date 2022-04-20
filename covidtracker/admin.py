e58e358fe8d790019cd20e97f07aeac5a012c9c4
panosit <panoskourpas@gmail.com>
2022-04-20 13:52:55 +0300

    make it better

--- covidtracker/admin.py
+++ covidtracker/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import *
 
 admin.site.register(Amphitheater)
+admin.site.register(Course)
 admin.site.register(Lecture)
 admin.site.register(Position)
 admin.site.register(CovidCase)
-admin.site.register(Course)
